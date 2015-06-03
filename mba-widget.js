128c63f22721288dee0d6cd7bcf75d98de0f572d
Manuel Spuhler <manuel.spuhler@gmail.com>
2015-06-03 18:13:23 +0200

    crop

--- mba-widget.js
+++ mba-widget.js
@@ -3,7 +3,7 @@
 // © 2015 Coteries SA
 // Manuel Spuhler
 
-/*global console, document, $ */
+/*global document, jQuery, window */
 
 var WIDGET = {
     url: function (q) {
@@ -25,28 +25,23 @@ var WIDGET = {
     callbackUrl: function () {
         "use strict";
         return [
-            "https://extapi.local.ch/en/0/tel/analytics",
-            "?lang=en",
-            "&user=bea25809bdf6ad3d1a3db21a22a5ffd5ca7788cf",
-            "&tid=v1_130811_1376258400000_bea25809bdf6ad3d1a3db21a22a5ffd5ca7788cf_a80a_928f",
-            "&v=1.0",
-            "&key=F1AD7B165AA149A2AE60374847902A12",
-            "&platform=unknown"
+            "https://extapi.local.ch/en/0/tel/analytics"
         ].join("");
+    },
+    db: {
+        views: []
     }
 };
 
 WIDGET.init = function () {
     "use strict";
-    console.log("init");
-
     var scriptPram = document.getElementById("mba-widget");
     var q = scriptPram.getAttribute("data-q");
+    var hasJumbotron = scriptPram.getAttribute("data-jumbotron") === "true" || false;
 
     WIDGET.controller.appendStyle();
     WIDGET.controller.appendLib(function () {
-        console.log("callback");
-        WIDGET.controller.process(WIDGET.url(q));
+        WIDGET.controller.process(WIDGET.url(q), hasJumbotron);
     });
 };
 
@@ -54,155 +49,132 @@ document.body.onload = WIDGET.init;
 
 WIDGET.controller = {
 
-    process: function (url) {
+    process: function (url, hasJumbotron) {
         "use strict";
-        $("#mba-widget").before("<div id='mba-widget-id'></div>");
-        $.getJSON(url, function (data) {
+        jQuery("#mba-widget").before("<div id='mba-widget-id'></div>");
+        jQuery.getJSON(url, function (data) {
             var items = [];
-          $.each( data.results, function (key, result) {
-            $.each( result.content_ads.mba_offers, function (offerKey, offerObj) {
-                if (offerObj === undefined) {
-                    return;
-                }
+              jQuery.each( data.results, function (index, result) {
+                jQuery.each( result.content_ads.mba_offers, function (offerIndex, offerObj) {
 
-                var offer = {
-                        name: result.title,
-                        // website: result.
-                        title: offerObj.title,
-                        description: offerObj.description,
-                        image: offerObj.photo,
-                        date: new Date(offerObj.expiry_ts * 1000),
-                        views: offerObj.views_no
-                };
+                    if (offerObj === undefined) {
+                        return;
+                    }
 
-                var theDate = offer.date;
-                var formattedDate = [
-                    "0" + theDate.getDate(),
-                    "0" + (theDate.getMonth() + 1),
-                    theDate.getFullYear()
-                ].map(function(v) {
-                    return (typeof v === "string") ? v.substr(v.length - 2) : v;
+                    var url = result.content_ads && result.content_ads.logo && result.content_ads.logo.url || null;
+
+                    var offer = {
+                            offerId: offerObj.id,
+                            name: result.title,
+                            title: offerObj.title,
+                            url: url,
+                            description: offerObj.description,
+                            image: offerObj.photo,
+                            date: new Date(offerObj.expiry_ts * 1000),
+                            views: offerObj.views_no
+                    };
+                    var theDate = offer.date;
+                    var formattedDate = [
+                        "0" + theDate.getDate(),
+                        "0" + (theDate.getMonth() + 1),
+                        theDate.getFullYear()
+                    ].map(function(v) {
+                        return (typeof v === "string") ? v.substr(v.length - 2) : v;
+                    });
+
+                    var subject = encodeURIComponent(offer.name + ": " + offer.title);
+                    var body = encodeURIComponent(offer.name + ": " + offer.title + ",  " + offer.description);
+                    var fbURL = "http://www.facebook.com/sharer.php?u=" + offer.url;
+                    var twURL = "https://twitter.com/intent/tweet?url=" + offer.url + "&text=" + body;
+
+                    var html = "";
+                    html += "  <div data-offer-id='" + offer.offerId + "' class='mba-offer' id='mba-offer-" + offerIndex + "'>";
+                    html += "    <div class='crop'>";
+                    html += "      <img width='120px' src='" + offer.image + "' />";
+                    html += "    </div>";
+                    html += "    <h4>" + offer.title + "</h4>";
+                    html += offer.description ? "<p>" + offer.description + "</p>" : "";
+                    html += "    <p>G&#252;ltig bis: " + formattedDate.join(".");
+                    html += ", " + offer.views + " Aufrufe";
+                    html += "    </p>";
+                    html += "    <ul class='mba-share'>";
+                    html += "      <li class='link1'><a href='mailto:?subject=" + subject + "&body=" + encodeURIComponent(offer.description + "\n") + offer.url + "'></a></li>";
+                    html += "      <li class='link2'>";
+                    html += "        <a id='tw-share' href='javascript: void(0);' onClick='WIDGET.controller.openWindow(\"" + twURL + "\")' target='_blank'></a>";
+                    html += "      </li>";
+                    html += "      <li class='link3' title='Facebook - share'>";
+                    html += "        <a id='fb-share' href='javascript: void(0);' onClick='WIDGET.controller.openWindow(\"" + fbURL + "\")' target='_blank'></a>";
+                    html += "      </li>";
+                    html += "     </ul>";
+                    html += "  </div>";
+
+                    items.push(html);
                 });
-
-                var html = "";
-                html += "<div class='mba-offer' id='" + offerKey + "'>";
-                html += "  <img width='120px' src='" + offer.image + "' />";
-                html += "  <p>" + offer.title + "</p>";
-                html += offer.description ? "<p>" + offer.description + "</p>" : "";
-                html += "  <p>Gültig bis: " + formattedDate.join(".");
-                html += ", " + offer.views + " Aufrufe";
-                html += "  </p>";
-
-                var subject = offer.name + ": " + offer.title;
-
-                html += "  <ul id='mba-share'>";
-                html += "    <li class='link1'><a href='mailto:?subject=" + subject + "&body=" + offer.description + "'></a></li>";
-                html += "    <li class='link2'><a href='#'></a></li>";
-                html += "    <li class='link3' title='Facebook - share'><a href='http://www.facebook.com/sharer.php?u=http://www.casaferlin.ch' target='_blank'></a></li>";
-                html += "  </ul>";
-
-                html += "</div>";
-                items.push(html);
             });
-          });
 
-          $("#mba-widget-id").append("<h3>Sonderangebote und Neuigkeiten</h3>");
-          $("#mba-widget-id").append(items.join(""));
+            var root = "<h3>Sonderangebote und Neuigkeiten</h3>";
+            root += "<div id='mba-offer-wrapper'>";
+            root += items.join("");
+            root += "</div>";
 
-          // TODO add this to the loop
-          // $(".mba-offer").click(function (e) {
-          //   console.log("clicked");
-          //   e.preventDefault();
-          //   var imageHref = $(this).find("img").attr("src");
+            jQuery("#mba-widget-id").append(root);
 
+            if (hasJumbotron) {
+                var offerElement = jQuery(".mba-offer").first();
+                offerElement.addClass("selected");
+                var offerId = offerElement.data("offer-id");
+                WIDGET.controller.markOfferAsViewed(offerId);
+            }
 
-          //   if ($("#lightbox").length > 0) { // #lightbox exists
-          //       $("#content").html("<img src='" + imageHref + "' />");
-          //       $("#lightbox").show();
-          //   } else { // create lightbox
-          //       var lightbox =
-          //           "<div id='lightbox'>" +
-          //               "<p>Click to close</p>" +
-          //               "<div id='content'>" +
-          //                   "<h2>Title</h2>" + 
-          //                   "<img src='" + imageHref + "' />" +
-          //                   "<div style='margin-top: 25px'>" +
-          //                   "<p>Lots of informations</p>" +
-          //                   "<p>Lots of informations</p>" +
-          //                   "<p>Lots of informations</p>" +
-          //                   "<p>Lots of informations</p>" +
-          //                   "</div>" +
-          //               "</div>" +
-          //           "</div>";
+            jQuery(".mba-offer").on("click", function () {
+                var isSelected = jQuery(this).hasClass("selected");
+                jQuery(".mba-offer").removeClass("selected");
 
-          //       $("body").append(lightbox);
-
-          //       $("#lightbox").on("click", function() {
-          //           $("#lightbox").hide();
-          //       });
-          //   }
-          // });
-
+                if (!(isSelected)) {
+                    jQuery(this).addClass("selected");
+                    var offerId = jQuery(this).data("offer-id");
+                    WIDGET.controller.markOfferAsViewed(offerId);
+                }
+            });
         });
-
-        // httpRequest = new XMLHttpRequest(); // TODO ie
-        // httpRequest.open('GET', url);
-        // httpRequest.onreadystatechange = function () {
-        //  if (httpRequest.readyState === 4) {
-     //             if (httpRequest.status === 200) {
-     //             WIDGET.controller.parse(httpRequest.responseText);
-     //             } else {
-     //             // TODO
-     //             }
-     //     }
-        // };
-        // httpRequest.send();
     },
 
-    // parse: function(responseText) {
-    //  var obj = JSON.parse(responseText);  // TODO json2.js
-    //  var results = obj['results'];
-    //  var offers = [];
-
-    //  var count = results.length;
-    //  for (var i = 0; i < count; i++) {
-    //      var result = results[i];
-    //      var offer = {
-    //          title: result['title'],
-    //          imageURL: result['icons']['primary']
-    //      };
-    //      WIDGET.controller.createElement(offer);
-    //  }
-    // },
-
-    // createElement: function(data) {
-    //  var newElement = document.createElement('div');
-    //  newElement.className = 'mba-offer';
-    //  var title = document.createTextNode(data.title);
-    //  var img = document.createElement('img');
-    //  img.src = data.imageURL;
-    //  var link = document.createElement('a');
-
-    //  newElement.appendChild(img);
-    //  newElement.appendChild(title);
-    //  var element = document.getElementById('mba-widget');
-    //  element.appendChild(newElement);
-    // },
-
-    popupElement: function () {
+    markOfferAsViewed: function (offerId){
         "use strict";
+        var data = {
+            category: "MBA_OFFER",
+            action: "view",
+            ts: 1432714809,
+            value: offerId
+        };
+        var views = WIDGET.db.views;
+        if (jQuery.inArray(offerId, views) === -1) {
+            views.push(offerId);
+            // jQuery.post(WIDGET.callbackUrl(), data);
+        }
+    },
+
+    openWindow: function (url) {
+        "use strict";
+        var winHeight = 350, winWidth = 520;
+        var winTop = (window.screen.height / 2) - (winHeight / 2);
+        var winLeft = (window.screen.width / 2) - (winWidth / 2);
+        window.open(url, "share", "top=" + winTop + ",left=" + winLeft + ",toolbar=0,status=0,width=" + winWidth + ",height=" + winHeight);
     },
 
     appendLib: function (callback) {
-        // TODO Check for jQuery before
+        // check for min version $.fn.jquery
+        // change scope var $jq = jQuery.noConflict(true);
         // TODO check for a smaller lib?
         "use strict";
-        var head = document.getElementsByTagName("head")[0];
-        var script = document.createElement("script");
-        script.type = "text/javascript";
-        script.onload = callback;
-        script.src = "//code.jquery.com/jquery-2.1.4.min.js";
-        head.appendChild(script);
+        if (typeof jQuery !== undefined) {
+            var head = document.getElementsByTagName("head")[0];
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.onload = callback;
+            script.src = "//code.jquery.com/jquery-2.1.4.min.js";
+            head.appendChild(script);
+        }
     },
 
     appendStyle: function () {
